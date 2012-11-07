30bd0b35577a4adab0425f2e98011b15da79ca6a
Chris Schwarz <cschwarz@thoughtworks.com>
2012-11-07 17:15:49 -0600

    Added search point finding and tests

--- js/rf.js
+++ js/rf.js
@@ -1,4 +1,11 @@
 /**
+ * TODO
+ * 1) Remove jQuery dependency (all page interaction should be outside this module)
+ * 2) Fix fillGaps to deal with condition where no points left between search points
+ *    that are too far apart
+ */
+
+/**
  * Route Finder module
  */
 var routeFinder = function() {
@@ -23,8 +30,8 @@ var routeFinder = function() {
     self.transitLayer = new google.maps.TransitLayer();
     self.directionsDisplay = new google.maps.DirectionsRenderer();
     self.directions = [];
-    self.searchRadius = 500;
-    self.numSearches = 15;
+    self.searchRadius = 450;
+    self.numSearches = 20;
     self.transitMode = google.maps.TravelMode.WALKING
     self.directionsDisplay.setMap(self.map);
     self.transitLayer.setMap(self.map);
@@ -60,30 +67,141 @@ var routeFinder = function() {
   }
 
   self.findCorners = function(path) {
-    path = path || self.directions;
-    
     var curHeading = google.maps.geometry.spherical.computeHeading(path[0], path[1]);
     console.log('***FINDING CORNERS***');
     console.log('Initial heading (0->1): '+curHeading);
-    self.corners = [];
+    var corners = [];
     for(i=1; i<path.length-1; i++) {
       var newHeading = google.maps.geometry.spherical.computeHeading(path[i], path[i+1]);
       var delta = Math.min(Math.abs(newHeading-curHeading),Math.abs(360-newHeading+curHeading),Math.abs(360-curHeading+newHeading));
       curHeading = newHeading;
       console.log('New heading ('+i+'->'+(i+1)+'): '+newHeading+' (del='+delta+')');
-      if(delta > 35) {
-        self.corners.push(i);
+      if(delta > 35) { // 35 deg threshold
+        corners.push(i);
         console.log('Corner found at '+i);
       }
     }
+    console.log('***CORNERS FOUND***');
+    return corners;
+  }
+
+  self.determineSearchPoints = function(path) {
+    console.log('***DETERMINING SEARCH POINTS***');
+    console.log(self.searchRadius);
+    var minDist = self.searchRadius / 1.25;
+    var maxDist = (self.searchRadius * 2) - 0.1 * self.searchRadius;
+    console.log('(min, max): ('+minDist+', '+maxDist+')');
+    var corners = self.findCorners(path);
+    var result = [];
+    result.push(0);
+    
+    console.log('Adding corners...');
+    result = addCorners(path, result, corners, minDist);
+    console.log('...done.');
+
+    result.push(path.length-1); // Need an endpoint, else we might miss the end of the route
+
+    console.log('Filling in gaps...');
+    result = fillGaps(path, result, maxDist);
+    console.log('...done.');
+
+    console.log('***SEARCH POINTS DETERMINED***');
+    console.log(result.toString());
+    return result;
+  }
+
+  function addCorners(path, searchPoints, corners, minDist) {
+    for(i=0; i<corners.length; i++) {
+      var distance = google.maps.geometry.spherical.computeDistanceBetween(path[corners[i]], path[searchPoints.slice(-1)[0]]);
+      console.log(i+'->'+(i+1)+' distance: '+distance);
+      if(distance > minDist) {
+        searchPoints.push(corners[i]);
+        console.log(i+' added');
+      }
+    }
+    return searchPoints;
+  }
+
+  // Algorithm: 1) Look for gaps between consecutive points, 2) Add search point closest to middle of gap,
+  // 3) If any points were added, repeat.
+  // Naively protected against "large gap & no points left" scenario (will simply skip gap)
+  function fillGaps(path, searchPoints, maxDist) {
+    var repeat = false, addedSearchPoints = [];
+    for(i=0; i<searchPoints.length-1; i++) {
+      var distance = google.maps.geometry.spherical.computeDistanceBetween(path[searchPoints[i]], path[searchPoints[i+1]]);
+      console.log(i+'->'+(i+1)+' distance: '+distance);
+      if(distance > maxDist && searchPoints[i+1]-searchPoints[i] > 1) {
+        repeat = true;
+        var bestFiller = 0, bestDiff = distance, lb = searchPoints[i], ub = searchPoints[i+1];
+        for(j=lb; j<ub; j++) {
+          var d1 = google.maps.geometry.spherical.computeDistanceBetween(path[lb], path[j]);
+          var d2 = google.maps.geometry.spherical.computeDistanceBetween(path[j], path[ub]);
+          if(Math.abs(d1-d2) < bestDiff) {
+            bestDiff = Math.abs(d1-d2);
+            bestFiller = j;
+          }
+        }
+        addedSearchPoints.push(bestFiller);
+        console.log(bestFiller+' added to fill gap');
+      }
+    }
+    searchPoints = searchPoints.concat(addedSearchPoints).sort(function(x, y) { return x-y; });
+    if(repeat) {
+      console.log('Need to rescan...')
+      searchPoints = fillGaps(path, searchPoints, maxDist);
+    }
+    return searchPoints;
   }
 
   // Determine size of search circles
   self.setSearchRadius = function(numSearches, distance) {
     console.log("ROUTE DISTANCE: "+distance);
     var newRadius = Math.floor(distance/numSearches);
-    self.searchRadius = Math.max(newRadius, 500); // 500 seems the best minimum
+    self.searchRadius = Math.max(newRadius, 450); // 450 seems the best minimum
   }
+
+// DEBUG METHODS
+  self.markers = [];
+  self.circles = [];
+  self.showDirectionsMarkers = function() {
+    for(var i=0; i<self.markers.length; i++) { self.markers[i].setMap(null); }
+    self.markers = [];
+    for(var i=0; i<self.circles.length; i++) { self.circles[i].setMap(null); }
+    self.circles = [];
+    for(var i=0; i<self.directions.length; i++) {
+      self.markers.push(new google.maps.Marker({
+        position: self.directions[i],
+        map: self.map,
+        title: ''+i
+      }));
+      self.attachMessage(self.markers[i], '#'+i);
+    }
+    var searchPoints = self.determineSearchPoints(self.directions);
+    for(var i=0; i<searchPoints.length; i++) {
+      self.circles.push(new google.maps.Circle({
+        strokeColor: "#0000FF",
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: "#0000FF",
+        fillOpacity: 0.2,
+        map: self.map,
+        center: self.directions[searchPoints[i]],
+        radius: self.searchRadius
+      }));
+    }
+  }
+
+  self.attachMessage = function(marker, theMessage) {
+    var message = theMessage;
+    var infowindow = new google.maps.InfoWindow(
+      { content: message,
+        size: new google.maps.Size(1,1)
+      });
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.open(self.map, marker);
+    });
+  };
+// --END DEBUG METHODS
 
 // THESE NEED TO BE REFACTORED OUT OF THE MODULE
 
@@ -108,31 +226,6 @@ var routeFinder = function() {
     element.fadeIn(100);
     pendingTimeouts.push(setTimeout(function() { element.fadeOut(100); }, 2000));
   }
-
-  self.markers = new Array();
-  self.showDirectionsMarkers = function() {
-    for(var i=0; i<self.markers.length; i++) { self.markers[i].setMap(null); }
-    self.markers = new Array();
-    for(var i=0; i<self.directions.length; i++) {
-      self.markers.push(new google.maps.Marker({
-        position: self.directions[i],
-        map: self.map,
-        title: ''+i
-      }));
-      self.attachMessage(self.markers[i], '#'+i);
-    }
-  }
-
-  self.attachMessage = function(marker, theMessage) {
-    var message = theMessage;
-    var infowindow = new google.maps.InfoWindow(
-      { content: message,
-        size: new google.maps.Size(1,1)
-      });
-    google.maps.event.addListener(marker, 'click', function() {
-      infowindow.open(self.map, marker);
-    });
-  };
 // --END BAD METHODS--
 
   return {init: init};
